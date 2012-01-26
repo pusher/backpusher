63b06d69a1c57c7aecacc65054ff5e4bacc9ddb7
Monica Olinescu <monica.olinescu@gmail.com>
2012-01-26 14:33:29 -0500

    Checking to see if Pusher is already connected when fetching the socket_id

--- src/backpusher.js
+++ src/backpusher.js
@@ -15,9 +15,13 @@
     // Bind for the connection established, so
     // we can setup the socket_id param.
     if (channel.pusher.connection) {
-      channel.pusher.connection.bind('connected', function() {
+      if(channel.pusher.connection.socket_id) {
         Backbone.pusher_socket_id = channel.pusher.connection.socket_id;
-      });
+      } else {
+        channel.pusher.connection.bind('connected', function() {
+          Backbone.pusher_socket_id = channel.pusher.connection.socket_id;
+        });
+      }
     } else {
       channel.pusher.bind('pusher:connection_established', function() {
         Backbone.pusher_socket_id = channel.pusher.socket_id;
