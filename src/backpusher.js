388500f9b7912b5e1ca3b97b0843269c5f404c9f
Mark Dodwell <mark@mkdynamic.co.uk>
2012-03-07 11:11:00 -0800

    set socket_id header on backbone sync requests without overriding the whole sync function

--- src/backpusher.js
+++ src/backpusher.js
@@ -100,55 +100,15 @@
     'read'  : 'GET'
   };
 
+  // Add socket ID to every Backbone.sync request
+  var origBackboneSync = Backbone.sync;
   Backbone.sync = function(method, model, options) {
-    var type = methodMap[method];
+    options.headers = _.extend(
+      { 'X-Pusher-Socket-ID': Backbone.pusher_socket_id },
+      options.headers
+    );
 
-    // Default JSON-request options.
-    var params = _.extend({
-      type:         type,
-      dataType:     'json'
-    }, options);
-
-    if (!(model && model.url)) {
-      throw new Error("A 'url' property or function must be specified");
-    }
-
-    if (!params.url) {
-      params.url = _.isFunction(model.url) ? model.url() : model.url;
-      params.url += '?socket_id=' + Backbone.pusher_socket_id;
-    }
-
-    // Ensure that we have the appropriate request data.
-    if (!params.data && model && (method == 'create' || method == 'update')) {
-      params.contentType = 'application/json';
-      params.data = JSON.stringify(model.toJSON());
-    }
-
-    // For older servers, emulate JSON by encoding the request into an HTML-form.
-    if (Backbone.emulateJSON) {
-      params.contentType = 'application/x-www-form-urlencoded';
-      params.data        = params.data ? {model : params.data} : {};
-    }
-
-    // For older servers, emulate HTTP by mimicking the HTTP method with `_method`
-    // And an `X-HTTP-Method-Override` header.
-    if (Backbone.emulateHTTP) {
-      if (type === 'PUT' || type === 'DELETE') {
-        if (Backbone.emulateJSON) params.data._method = type;
-        params.type = 'POST';
-        params.beforeSend = function(xhr) {
-          xhr.setRequestHeader('X-HTTP-Method-Override', type);
-        };
-      }
-    }
-
-    // Don't process data on a non-GET request.
-    if (params.type !== 'GET' && !Backbone.emulateJSON) {
-      params.processData = false;
-    }
-
-    // Make the request.
-    return $.ajax(params);
+    origBackboneSync(method, model, options);
   };
 
   // Export:
